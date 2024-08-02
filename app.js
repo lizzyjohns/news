c9a33b0290b68f4600cc8217dd2c60f20fbc0fce
georgepayne2018 <georgepayn2018@gmail.com>
2024-08-02 22:29:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ async function main () {
         // Send the authorization URL back to the site
         const authorizationUrl = `https://api.twitter.com/oauth/authorize?oauth_token=${oauthToken}`;
         //res.json({ authorization_url: authorizationUrl });
-    });
+    
         req.session = req.session || {};
         req.session.oauthRequestToken = oauthRequestToken;
         req.session.oauthRequestTokenSecret = oauthRequestTokenSecret;
